bc45e725091787f92a147f42c9666f8ec83d3d07
Luis Capelo <luiscape@gmail.com>
2015-01-08 19:11:21 -0500

    collecting live data from the who

---
+++ code/scraper.R
@@ -0,0 +1,153 @@
+## Script to download data from the WHO, 
+## parse it, and store it in a database.
+
+library(RCurl)
+library(rjson)  # may not need it
+
+# SW helper function
+onSw <- function(d = F, l = "tool/") {
+  if (d) return(l)
+  else return("")
+}
+
+# Helper functions
+source(paste0(onSw(), 'code/write_tables.R'))
+source(paste0(onSw(), 'code/sw_status.R'))
+
+# Function to query the WHO API and download
+# the file locally.
+getWHOFile <- function() {
+  # building url using the current date
+  current_date <- Sys.Date()
+  date_url <- paste0('http://apps.who.int/gho/athena/xmart/data-coded.csv?target=EBOLA_MEASURE/CASES,DEATHS&filter=LOCATION:-;DATAPACKAGEID:',current_date,';INDICATOR_TYPE:SITREP_CUMULATIVE;INDICATOR_TYPE:SITREP_CUMULATIVE_21_DAYS;SEX:-')
+  
+  # downloading the resulting file in a local
+  # temporary csv file
+  file_path = paste0(onSw(), 'data/temp.csv')
+  download.file(date_url, file_path, method = "wget", quiet = F)
+  
+  # return message
+  return(file_path)
+}
+
+# Parse the WHO data into a data.frame that
+# follows the format used in HDX.
+parseData <- function(file_path = getWHOFile()) {
+  # loading data
+  data <- read.csv(file_path)
+  
+  # cleaning
+  data$EPI_DATE <- NULL  # we use the reporting date
+  data$COUNTRY <- as.character(data$COUNTRY)
+  data$CASE_DEFINITION <- as.character(data$CASE_DEFINITION)
+  data$Display.Value <- NULL
+  data$Low <- NULL
+  data$High <- NULL
+  data$INDICATOR_TYPE <- NULL
+  data$Comments <- NULL
+  data <- subset(data, data$COUNTRY != "UNSPECIFIED")
+  data$CASE_DEFINITION <- ifelse(data$CASE_DEFINITION == "", NA, data$CASE_DEFINITION)
+  
+  # transforming
+  data$COUNTRY <- ifelse(data$COUNTRY == 'MLI', "Mali", data$COUNTRY)
+  data$COUNTRY <- ifelse(data$COUNTRY == 'GBR', "United Kingdom", data$COUNTRY)
+  data$COUNTRY <- ifelse(data$COUNTRY == 'LBR', "Liberia", data$COUNTRY)
+  data$COUNTRY <- ifelse(data$COUNTRY == 'GIN', "Guinea", data$COUNTRY)
+  data$COUNTRY <- ifelse(data$COUNTRY == 'SLE', "Sierra Leone", data$COUNTRY)
+  
+  # create indicators
+  createIndicators <- function(df = data) {
+    # creating variable
+    df$Indicator <- NA
+    
+    ## Creating conditions for each indicator
+    # Cases
+    df$Indicator <- ifelse(
+      df$EBOLA_MEASURE == 'CASES' & df$CASE_DEFINITION == 'CONFIRMED',
+      'Cumulative number of confirmed Ebola cases',
+      df$Indicator
+    )
+    df$Indicator <- ifelse(
+      df$EBOLA_MEASURE == 'CASES' & df$CASE_DEFINITION == 'PROBABLE',
+      'Cumulative number of probable Ebola cases',
+      df$Indicator
+    )
+    df$Indicator <- ifelse(
+      df$EBOLA_MEASURE == 'CASES' & df$CASE_DEFINITION == 'SUSPECTED',
+      'Cumulative number of suspected Ebola cases',
+      df$Indicator
+    )
+    df$Indicator <- ifelse(
+      df$EBOLA_MEASURE == 'CASES' & df$CASE_DEFINITION == 'CONF_PROB_SUSP',
+      'Cumulative number of confirmed, probable and suspected Ebola cases',
+      df$Indicator
+    )
+    
+    # Deaths
+    df$Indicator <- ifelse(
+      df$EBOLA_MEASURE == 'DEATHS' & df$CASE_DEFINITION == 'CONFIRMED',
+      'Cumulative number of confirmed Ebola deaths',
+      df$Indicator
+    )
+    df$Indicator <- ifelse(
+      df$EBOLA_MEASURE == 'DEATHS' & df$CASE_DEFINITION == 'PROBABLE',
+      'Cumulative number of probable Ebola deaths',
+      df$Indicator
+    )
+    df$Indicator <- ifelse(
+      df$EBOLA_MEASURE == 'DEATHS' & df$CASE_DEFINITION == 'SUSPECTED',
+      'Cumulative number of suspected Ebola deaths',
+      df$Indicator
+    )
+    df$Indicator <- ifelse(
+      df$EBOLA_MEASURE == 'DEATHS' & df$CASE_DEFINITION == 'CONF_PROB_SUSP',
+      'Cumulative number of confirmed, probable and suspected Ebola deaths',
+      df$Indicator
+    )
+    df$Indicator <- ifelse(
+      df$EBOLA_MEASURE == 'DEATHS' & is.na(df$CASE_DEFINITION),  # exception
+      'Cumulative number of confirmed, probable and suspected Ebola deaths',
+      df$Indicator
+    )
+    
+    ## Two groups missing:
+    # - number of cases in the last 21 days
+    # - proportion of new cases of the last 21 days
+    
+    df$EBOLA_MEASURE <- NULL
+    df$CASE_DEFINITION <- NULL
+
+    return(df)
+  }
+  
+  data <- createIndicators()
+  names(data) <- c("Country", "Date", "value", "Indicator")
+  
+  return(data)
+}
+
+# Scraper wrapper
+runScraper <- function() {
+  cat('-----------------------------\n')
+  cat('Collecting current data.\n')
+  data <- parseData()
+  writeTable(data, 'who_ebola_case_data', 'scraperwiki')
+  cat('Done.\n')
+  cat('-----------------------------\n')
+}
+
+# Scraperwiki error handler.
+runScraper()
+
+# Changing the status of SW.
+# tryCatch(runScraper(),
+#          error = function(e) {
+#            cat('Error detected ... sending notification.')
+#            system('mail -s "WFP VAM figures failed." luiscape@gmail.com')
+#            changeSwStatus(type = "error", message = "Scraper failed.")
+# { stop("!!") }
+#          }
+# )
+# 
+# # If success:
+# changeSwStatus(type = 'ok')
