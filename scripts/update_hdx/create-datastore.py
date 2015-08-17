97d0889f39bb2d002c0e70adf83fa33beb32bcac
Luis Capelo <luiscape@gmail.com>
2015-08-17 16:16:32 -0400

    linting and improving comments

--- scripts/update_hdx/create-datastore.py
+++ scripts/update_hdx/create-datastore.py
@@ -1,174 +1,176 @@
-# Simple script that manages the creation of
-# datastores in CKAN / HDX.
-
 import os
 import csv
-import json
-import scraperwiki
-import ckanapi
-import urllib
-import requests
 import sys
+import json
+import urllib
+import ckanapi
 import hashlib
+import requests
+import scraperwiki
 
 # Collecting configuration variables
 PATH = 'tool/data/ebola-data-db-format.csv'
-remote = 'https://data.hdx.rwlabs.org'
+REMOTE_CKAN = 'https://data.hdx.rwlabs.org'
 resource_id = sys.argv[1]
 apikey = sys.argv[2]
 
 # ckan will be an instance of ckan api wrapper
 ckan = None
 
-# Function to download a resource from CKAN.
-def downloadResource(filename):
 
-    print "Downloading file from CKAN."
+def downloadResource(filename, resource_id):
+  '''Downloads a resource from CKAN.'''
 
-    # querying
-    url = 'https://data.hdx.rwlabs.org/api/action/resource_show?id=' + resource_id
-    r = requests.get(url)
-    doc = r.json()
-    fileUrl = doc["result"]["perma_link"]
+  print "Downloading file from CKAN."
 
-    # downloading
-    try:
-        urllib.urlretrieve(fileUrl, filename)
-    except:
-        print 'There was an error downlaoding the file.'
+  #
+  # Querying HDX for download URL.
+  #
+  url = REMOTE_CKAN + '/api/action/resource_show?id=' + resource_id
+  r = requests.get(url)
+  doc = r.json()
+  fileUrl = doc["result"]["perma_link"]
 
-def uploadResource(resource_id, apikey, p):
-    hdx = ckanapi.RemoteCKAN('https://data.hdx.rwlabs.org',
-        apikey=apikey,
-        user_agent='CKAN_API/1.0')
-    try:
-        hdx.action.resource_update(
-            id=resource_id,
-            upload=open(p),
-            format='CSV',
-            description='Ebola data in record format with indicator, country, date and value.'
-            )
+  #
+  # Downloads file.
+  #
+  try:
+    with open(filename, 'wb') as f:
+      for chunk in r:
+        f.write(chunk)
 
-    except ckanapi.errors.ValidationError:
-            print 'You have missing parameters. Check the url and type are included.\n'
-
-    except ckanapi.errors.NotFound:
-            print 'Resource not found!\n'
+  except Exception as e:
+    print e
+    print 'There was an error downlaoding the file.'
+    return False
 
 
+#
 # Function that checks for old SHA hash
 # and stores as a SW variable the new hash
 # if they differ. If this function returns true,
 # then the datastore is created.
+#
 def checkHash(filename, first_run):
-    hasher = hashlib.sha1()
-    with open(filename, 'rb') as afile:
-        buf = afile.read()
-        hasher.update(buf)
-        new_hash = hasher.hexdigest()
+  ''' Checks hash of file.'''
 
-    # checking if the files are identical or if
-    # they have changed
-    if first_run:
-        scraperwiki.sqlite.save_var('datastore', new_hash)
-        new_data = False
+  hasher = hashlib.sha1()
+  with open(filename, 'rb') as afile:
+    buf = afile.read()
+    hasher.update(buf)
+    new_hash = hasher.hexdigest()
 
-    else:
-        old_hash = scraperwiki.sqlite.get_var('datastore')
-        scraperwiki.sqlite.save_var('datastore', new_hash)
-        new_data = old_hash != new_hash
+  #
+  # Checking if the files are identical or if
+  # they have changed.
+  #
+  if first_run:
+    scraperwiki.sqlite.save_var('datastore', new_hash)
+    new_data = False
 
-    # returning a boolean
-    return new_data
+  else:
+    old_hash = scraperwiki.sqlite.get_var('datastore')
+    scraperwiki.sqlite.save_var('datastore', new_hash)
+    new_data = old_hash != new_hash
+
+  #
+  # Returning a Boolean.
+  #
+  return new_data
+
 
 def updateDatastore(filename):
-    print "Updating DataStore ..."
+  '''Updates a CKAN DataStore based on CSV input.'''
 
-    # Checking if there is new data
-    new_data = checkHash(filename, first_run = False)
-    if (new_data == False):
-        print "DataStore Status: No new data. Not updating datastore."
-        return
+  print "Updating DataStore ..."
 
-    else:
-        print "DataStore Status: New data. Updating datastore."
+  #
+  # Checking the hashes of files to
+  # see if an update is necessary.
+  #
+  new_data = checkHash(filename, first_run = False)
+  if (new_data == False):
+    print "DataStore Status: No new data. Not updating datastore."
+    return False
 
-        # defining the schema
-        resources = [
-            {
-                'resource_id': resource_id,
-                'path': filename,
-                'schema': {
-                    "fields": [
-                      { "id": "Indicator", "type": "text" },
-                      { "id": "Country", "type": "text" },
-                      { "id": "Date", "type": "timestamp"},
-                      { "id": "value", "type": "float" }
-                    ]
-                },
-            }
-        ]
+  else:
+    print "DataStore Status: New data. Updating datastore."
 
-
-        def upload_data_to_datastore(ckan_resource_id, resource):
-            # let's delete any existing data before we upload again
-            try:
-                ckan.action.datastore_delete(resource_id=ckan_resource_id, force=True)
-            except:
-                pass
-
-            ckan.action.datastore_create(
-                    resource_id=ckan_resource_id,
-                    force=True,
-                    fields=resource['schema']['fields'],
-                    primary_key=resource['schema'].get('primary_key'))
-
-            reader = csv.DictReader(open(resource['path']))
-            rows = [ row for row in reader ]
-            chunksize = 10000
-            offset = 0
-            print('Uploading data for file: %s' % resource['path'])
-            while offset < len(rows):
-                rowset = rows[offset:offset+chunksize]
-                ckan.action.datastore_upsert(
-                        resource_id=ckan_resource_id,
-                        force=True,
-                        method='insert',
-                        records=rowset)
-                offset += chunksize
-                print('Done: %s' % offset)
+    #
+    # Resource schema.
+    #
+    resources = [
+        {
+          'resource_id': resource_id,
+          'path': filename,
+          'schema': {
+            "fields": [
+              { "id": "Indicator", "type": "text" },
+              { "id": "Country", "type": "text" },
+              { "id": "Date", "type": "timestamp"},
+              { "id": "value", "type": "float" }
+            ]
+          },
+        }
+    ]
 
 
-        if __name__ == '__main__':
-            if len(sys.argv) <= 2:
-                usage = '''python scripts/upload.py {resource-id} {api-key}
+    def upload_data_to_datastore(ckan_resource_id, resource):
+        '''Uploads data to a CKAN DataStore.'''
 
-                e.g.
+        #
+        # Deletes DataStore -- if it exists.
+        #
+        try:
+          ckan.action.datastore_delete(resource_id=ckan_resource_id, force=True)
+        except:
+          pass
 
-                python scripts/upload.py RESOURCE_ID API_KEY
-                '''
-                print(usage)
-                sys.exit(1)
+        ckan.action.datastore_create(
+                resource_id=ckan_resource_id,
+                force=True,
+                fields=resource['schema']['fields'],
+                primary_key=resource['schema'].get('primary_key'))
 
-            ckan = ckanapi.RemoteCKAN(remote, apikey=apikey)
+        reader = csv.DictReader(open(resource['path']))
+        rows = [ row for row in reader ]
+        chunksize = 10000
+        offset = 0
+        print('Uploading data for file: %s' % resource['path'])
+        while offset < len(rows):
+          rowset = rows[offset:offset+chunksize]
+          ckan.action.datastore_upsert(
+                  resource_id=ckan_resource_id,
+                  force=True,
+                  method='insert',
+                  records=rowset)
+          offset += chunksize
+          print('Done: %s' % offset)
 
-            resource = resources[0]
-            upload_data_to_datastore(resource['resource_id'], resource)
 
-def runEverything():
-    # uploadResource(resource_id, apikey, PATH)
-    downloadResource(PATH)
-    updateDatastore(PATH)
+def Main():
+  '''Wrapper.'''
+
+  ckan = ckanapi.RemoteCKAN(REMOTE_CKAN, apikey=apikey)
+  resource = resources[0]
+  upload_data_to_datastore(resource['resource_id'], resource)
+  downloadResource(PATH)
+  updateDatastore(PATH)
 
 
-# Error handler for running the entire script
-try:
+
+
+if __name__ == '__main__':
+  
+  #
+  # ScraperWiki error handler.
+  #
+  try:
     runEverything()
-    # if everything ok
     print "SW Status: Everything seems to be just fine."
     scraperwiki.status('ok')
 
-except Exception as e:
+  except Exception as e:
     print e
     scraperwiki.status('error', 'Creating datastore failed')
     os.system("mail -s 'Ebola Case data: creating datastore failed.' luiscape@gmail.com")
